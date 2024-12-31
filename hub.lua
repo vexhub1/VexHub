9947b1086e7299460e29f11a73ce453bc3d95322
VexHub <150626062+vexhub1@users.noreply.github.com>
2025-01-01 01:14:37 +0300

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,8 +1,8 @@
 repeat task.wait() until game:IsLoaded()
 
 local Games = {
-    [18901165922] = "github link here," -- soon
-    [8737899170] = "github link here", --ps99
+    [18901165922] = "https://raw.githubusercontent.com/vexhub1/VexHub/refs/heads/main/psgo.lua", -- psgo
+    [8737899170] = "https://raw.githubusercontent.com/vexhub1/VexHub/refs/heads/main/ps99.lua" --ps99
 }
 
 local scriptlink = Games[game.PlaceId]
