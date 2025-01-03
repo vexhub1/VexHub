f862316de63e96eea43fbd525be84f504fbe704e
VexHub <150626062+vexhub1@users.noreply.github.com>
2025-01-03 17:42:16 +0300

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,11 +1,1 @@
-repeat task.wait() until game:IsLoaded()
-
-local Games = {
-    [18901165922] = "https://raw.githubusercontent.com/vexhub1/VexHub/refs/heads/main/psgo.lua", -- psgo
-    [8737899170] = "https://raw.githubusercontent.com/vexhub1/VexHub/refs/heads/main/ps99.lua" --ps99
-}
-
-local scriptlink = Games[game.PlaceId]
-if scriptlink then
-    loadstring(game:HttpGet(scriptlink))()
-end
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vexhub1/VexHub/refs/heads/main/ps99.lua'))()
